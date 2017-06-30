224adff8496955fb7028abe5e4982879852fcf96
Johnny Choudhury-Lucas <johnny@mayden.co.uk>
2017-06-30 10:14:11 +0100

    up to CSS

--- js/clientApp.js
+++ js/clientApp.js
@@ -2,21 +2,28 @@ import React from 'react'
 import { render } from 'react-dom'
 import MyTitle from './MyTitle'
 
+/*
 var div = React.DOM.div
 var h1 = React.DOM.h1
 
 var MyTitleFactory = React.createFactory(MyTitle)
+*/
 
 var MyFirstComponent = React.createClass({
   render: function () {
     return (
-      div(null,
+      <div>
+        <MyTitle title='props are cool' color='rebeccapurple' />
+        <MyTitle title='props are amazing' color='mediumaquamarine' />
+        <MyTitle title='props are sweet' color='peru' />
+      </div>
+/*      div(null,
         h1(null, 'This is my first component'),
         MyTitleFactory({title: 'title 1', color: 'peru'}),
         MyTitleFactory({title: 'title 2', color: 'mediumaquamarine'}),
         MyTitleFactory({title: 'title 9', color: 'rebeccapurple'}),
         MyTitleFactory({title: 'title 42', color: 'darkvioletred'})
-      )
+      )*/
     )
   }
 })
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+  context: __dirname,
+  entry: './js/ClientApp.js',
+  devtool: 'source-map',
+  output: {
+    path: path.join(__dirname, '/public'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.json']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: false
+  },
+  module: {
+    rules: [
+      {
+        include: path.resolve(__dirname, 'js'),
+        // exclude: /node_modules/,
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
