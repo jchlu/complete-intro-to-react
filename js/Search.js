ad5e13a958cb34c40cb3766e1974e2644c1065ba
Johnny Choudhury-Lucas <jchlu+git@me.com>
2017-07-04 21:51:13 +0100

    Add state to the Search

--- js/Search.js
+++ js/Search.js
@@ -4,18 +4,28 @@ import ShowCard from './ShowCard'
 import preload from '../public/data.json'
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: 'this is the default string'
+    }
+  },
+  handleSearchTermChange (event) {
+    this.setState({searchTerm: event.target.value})
+  },
   render () {
     return (
       <div className='search'>
-        {/*
-         <h1>Search page!!</h1>
-         <input type='text' placeholder='Search' />
-         */}
-        {preload.shows.map((show) => {
-          return (
-            <ShowCard key={show.imdbID} {...show} />
-          )
-        })}
+        <header>
+          <h1>{this.state.searchTerm}</h1>
+          <input onChange={this.handleSearchTermChange} type='text' value={this.state.searchTerm} placeholder='Search' />
+        </header>
+        <div>
+          {preload.shows.map((show) => {
+            return (
+              <ShowCard key={show.imdbID} {...show} />
+            )
+          })}
+        </div>
         <Link to='/'>Home</Link>
       </div>
     )
